3ad7633e9886f4d6618137b60427190ddfe80144
Dunc1995 <duncanbailey1995@gmail.com>
2024-08-28 16:42:04 +1200

    removes unnecessary dicts from db migration

--- investorbot/db.py
+++ investorbot/db.py
@@ -8,75 +8,75 @@ from investorbot.models import CoinSelectionCriteria
 logger = logging.getLogger(DEFAULT_LOGS_NAME)
 
 
-HIGH_CONFIDENCE = {
-    "rating_id": ConfidenceRating.HIGH_CONFIDENCE.value,
-    "rating_description": "High Confidence",
-    "trade_needs_to_be_within_mean_and_upper_bound": False,
-    "trade_needs_to_be_within_mean_and_lower_bound": True,
-    "standard_deviation_threshold_should_exceed_threshold": True,
-    "standard_deviation_threshold": 0.03,
-    "trend_line_percentage_threshold": 0.005,
-    "trend_line_should_be_flat": False,
-    "trend_line_should_be_rising": True,
-    "trend_line_should_be_falling": False,
-    "trend_line_should_be_flat_or_rising": False,
-}
+HIGH_CONFIDENCE = CoinSelectionCriteria(
+    rating_id=ConfidenceRating.HIGH_CONFIDENCE.value,
+    rating_description="High Confidence",
+    trade_needs_to_be_within_mean_and_upper_bound=False,
+    trade_needs_to_be_within_mean_and_lower_bound=True,
+    standard_deviation_threshold_should_exceed_threshold=True,
+    standard_deviation_threshold=0.03,
+    trend_line_percentage_threshold=0.005,
+    trend_line_should_be_flat=False,
+    trend_line_should_be_rising=True,
+    trend_line_should_be_falling=False,
+    trend_line_should_be_flat_or_rising=False,
+)
 
-MODERATE_CONFIDENCE = {
-    "rating_id": ConfidenceRating.MODERATE_CONFIDENCE.value,
-    "rating_description": "Moderate Confidence",
-    "trade_needs_to_be_within_mean_and_upper_bound": False,
-    "trade_needs_to_be_within_mean_and_lower_bound": True,
-    "standard_deviation_threshold_should_exceed_threshold": True,
-    "standard_deviation_threshold": 0.025,
-    "trend_line_percentage_threshold": 0.001,
-    "trend_line_should_be_flat": False,
-    "trend_line_should_be_rising": True,
-    "trend_line_should_be_falling": False,
-    "trend_line_should_be_flat_or_rising": False,
-}
+MODERATE_CONFIDENCE = CoinSelectionCriteria(
+    rating_id=ConfidenceRating.MODERATE_CONFIDENCE.value,
+    rating_description="Moderate Confidence",
+    trade_needs_to_be_within_mean_and_upper_bound=False,
+    trade_needs_to_be_within_mean_and_lower_bound=True,
+    standard_deviation_threshold_should_exceed_threshold=True,
+    standard_deviation_threshold=0.025,
+    trend_line_percentage_threshold=0.001,
+    trend_line_should_be_flat=False,
+    trend_line_should_be_rising=True,
+    trend_line_should_be_falling=False,
+    trend_line_should_be_flat_or_rising=False,
+)
 
-UNDECIDED = {
-    "rating_id": ConfidenceRating.UNDECIDED.value,
-    "rating_description": "Unsure",
-    "trade_needs_to_be_within_mean_and_upper_bound": False,
-    "trade_needs_to_be_within_mean_and_lower_bound": True,
-    "standard_deviation_threshold_should_exceed_threshold": True,
-    "standard_deviation_threshold": 0.02,
-    "trend_line_percentage_threshold": 0.002,
-    "trend_line_should_be_flat": False,
-    "trend_line_should_be_rising": False,
-    "trend_line_should_be_falling": False,
-    "trend_line_should_be_flat_or_rising": True,
-}
+UNDECIDED = CoinSelectionCriteria(
+    rating_id=ConfidenceRating.UNDECIDED.value,
+    rating_description="Unsure",
+    trade_needs_to_be_within_mean_and_upper_bound=False,
+    trade_needs_to_be_within_mean_and_lower_bound=True,
+    standard_deviation_threshold_should_exceed_threshold=True,
+    standard_deviation_threshold=0.02,
+    trend_line_percentage_threshold=0.002,
+    trend_line_should_be_flat=False,
+    trend_line_should_be_rising=False,
+    trend_line_should_be_falling=False,
+    trend_line_should_be_flat_or_rising=True,
+)
 
-LITTLE_CONFIDENCE = {
-    "rating_id": ConfidenceRating.LITTLE_CONFIDENCE.value,
-    "rating_description": "Low Confidence",
-    "trade_needs_to_be_within_mean_and_upper_bound": False,
-    "trade_needs_to_be_within_mean_and_lower_bound": True,
-    "standard_deviation_threshold_should_exceed_threshold": True,
-    "standard_deviation_threshold": 0.02,
-    "trend_line_percentage_threshold": 0.005,
-    "trend_line_should_be_flat": False,
-    "trend_line_should_be_rising": False,
-    "trend_line_should_be_falling": False,
-    "trend_line_should_be_flat_or_rising": True,
-}
+LITTLE_CONFIDENCE = CoinSelectionCriteria(
+    rating_id=ConfidenceRating.LITTLE_CONFIDENCE.value,
+    rating_description="Low Confidence",
+    trade_needs_to_be_within_mean_and_upper_bound=False,
+    trade_needs_to_be_within_mean_and_lower_bound=True,
+    standard_deviation_threshold_should_exceed_threshold=True,
+    standard_deviation_threshold=0.02,
+    trend_line_percentage_threshold=0.005,
+    trend_line_should_be_flat=False,
+    trend_line_should_be_rising=False,
+    trend_line_should_be_falling=False,
+    trend_line_should_be_flat_or_rising=True,
+)
 
-NO_CONFIDENCE = {
-    "rating_id": ConfidenceRating.NO_CONFIDENCE.value,
-    "rating_description": "No Confidence - Do not Invest",
-    "trade_needs_to_be_within_mean_and_upper_bound": False,
-    "trade_needs_to_be_within_mean_and_lower_bound": False,
-    "standard_deviation_threshold_should_exceed_threshold": False,
-    "standard_deviation_threshold": 0.02,
-    "trend_line_percentage_threshold": 0.005,
-    "trend_line_should_be_flat": False,
-    "trend_line_should_be_rising": False,
-    "trend_line_should_be_falling": False,
-    "trend_line_should_be_flat_or_rising": False,
-}
+NO_CONFIDENCE = CoinSelectionCriteria(
+    rating_id=ConfidenceRating.NO_CONFIDENCE.value,
+    rating_description="No Confidence - Do not Invest",
+    trade_needs_to_be_within_mean_and_upper_bound=False,
+    trade_needs_to_be_within_mean_and_lower_bound=False,
+    standard_deviation_threshold_should_exceed_threshold=False,
+    standard_deviation_threshold=0.02,
+    trend_line_percentage_threshold=0.005,
+    trend_line_should_be_flat=False,
+    trend_line_should_be_rising=False,
+    trend_line_should_be_falling=False,
+    trend_line_should_be_flat_or_rising=False,
+)
 
 
 def init_db():
@@ -84,11 +84,11 @@ def init_db():
 
     coin_properties = crypto_service.get_coin_properties()
     market_analysis_ratings = [
-        CoinSelectionCriteria(**HIGH_CONFIDENCE),
-        CoinSelectionCriteria(**MODERATE_CONFIDENCE),
-        CoinSelectionCriteria(**UNDECIDED),
-        CoinSelectionCriteria(**LITTLE_CONFIDENCE),
-        CoinSelectionCriteria(**NO_CONFIDENCE),
+        HIGH_CONFIDENCE,
+        MODERATE_CONFIDENCE,
+        UNDECIDED,
+        LITTLE_CONFIDENCE,
+        NO_CONFIDENCE,
     ]
 
     app_service.add_items(coin_properties)
