eb3033fb20833e4c7d26901dd32a1adb278b0db7
Dunc1995 <duncanbailey1995@gmail.com>
2024-12-26 16:08:47 +0000

    Adds links to investorbot to inspect crypto service

--- investorbot/app.py
+++ investorbot/app.py
@@ -1,5 +1,5 @@
 import atexit
-from flask import Flask, json
+from flask import Flask, json, render_template
 from investorbot import app_service, crypto_service
 import investorbot.routines as routines
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -8,16 +8,21 @@ app = Flask(__name__)
 
 scheduler = BackgroundScheduler()
 
+
+def placeholder():
+    print("HELLO")
+
+
 # scheduler.add_job( func=routines.update_time_series_summaries_routine, trigger="interval",
 #     minutes=60, name="update_time_series_summaries_routine", )
 scheduler.add_job(
-    func=routines.buy_coin_routine,
+    func=placeholder,
     trigger="interval",
     minutes=3,
     name="buy_coin_routine",
 )
 scheduler.add_job(
-    func=routines.sell_coin_routine,
+    func=placeholder,
     trigger="interval",
     minutes=3,
     name="sell_coin_routine",
@@ -30,7 +35,23 @@ atexit.register(lambda: scheduler.shutdown())
 
 
 @app.route("/")
-def home():
-    orders = app_service.get_all_buy_orders()
+def index():
+    # return {"latest-trades": "http://127.0.0.1:5000/get-latest-trades"}
+    internal_links = ["/get-latest-trades"]
+    crypto_dot_com_links = ["/get-tickers"]
 
-    return [{"orderId": order.buy_order_id} for order in orders]
+    return render_template(
+        "index.html",
+        internal_links=internal_links,
+        api_links=crypto_dot_com_links,
+    )
+
+
+@app.route("/get-latest-trades")
+def get_latest_trades():
+    return crypto_service.get_latest_trades()
+
+
+@app.route("/get-tickers")
+def get_tickers():
+    return crypto_service.market.get_usd_tickers()
