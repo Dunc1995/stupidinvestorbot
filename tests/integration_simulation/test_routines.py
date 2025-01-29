d9de0ea65c31fe926f3b16983cb9fcca9bd3b4cd
Dunc1995 <duncanbailey1995@gmail.com>
2025-01-29 16:31:42 +0000

    adds buy order routine test to simulation tests. renames market analysis test for simulation

--- tests/integration_simulation/test_routines.py
+++ tests/integration_simulation/test_routines.py
@@ -1,8 +1,8 @@
 from investorbot.integrations.simulation.models import PositionBalanceSimulated
-from investorbot.routines import refresh_market_analysis_routine
+from investorbot.routines import buy_coin_routine, refresh_market_analysis_routine
 
 
-def test_wallet_tracks_usd_balance_with_orders(
+def test_market_analysis_can_be_executed_on_simulation(
     monkeypatch, mock_app_service, mock_simulated_crypto_service
 ):
     wallet_entry = PositionBalanceSimulated(
@@ -19,3 +19,22 @@ def test_wallet_tracks_usd_balance_with_orders(
 
     # TODO add some assertions for what to expect after running this
     refresh_market_analysis_routine(hours=24)
+
+
+def test_buy_order_routine_works_on_simulation(
+    monkeypatch, mock_app_service, mock_simulated_crypto_service
+):
+    wallet_entry = PositionBalanceSimulated(
+        coin_name="USD", market_value=100.0, quantity=100.0, reserved_quantity=0.0
+    )
+
+    mock_simulated_crypto_service.simulation_service.add_item(wallet_entry)
+
+    # Patch routine services with in-memory variants.
+    monkeypatch.setattr("investorbot.routines.app_service", mock_app_service)
+    monkeypatch.setattr(
+        "investorbot.routines.crypto_service", mock_simulated_crypto_service
+    )
+
+    # TODO add some assertions for what to expect after running this
+    buy_coin_routine()
